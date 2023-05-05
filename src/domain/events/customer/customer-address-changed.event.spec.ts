da9c7038777aac33d1228a43c710aebf6b788811
alanwcg <alancintra7@gmail.com>
2023-05-05 15:10:44 -0300

    feat: customer domain events

---
+++ src/domain/events/customer/customer-address-changed.event.spec.ts
@@ -0,0 +1,26 @@
+import { Address } from '../../entities/address'
+import { Customer } from '../../entities/customer'
+import { EventDispatcher } from '../@shared/event-dispatcher'
+import { CustomerAddressChangedEvent } from './customer-address-changed.event'
+import { SendConsoleLogHandler } from './handlers/send-console-log.handler'
+
+describe('Customer created event unit tests', () => {
+  it('should run ChangeCustomerAddressEvent handlers when a customer address is changed', () => {
+    const eventDispatcher = new EventDispatcher()
+    const eventHandler = new SendConsoleLogHandler()
+    const eventHandlerSpy = jest.spyOn(eventHandler, 'handle')
+
+    eventDispatcher.register('CustomerAddressChangedEvent', eventHandler)
+    const customer = new Customer('c1', 'Alan Cintra')
+    const address = new Address('Street 1', 1, 'Zipcode 1', 'City 1')
+    customer.changeAddress(address)
+    const customerAddressChangedEvent = new CustomerAddressChangedEvent({
+      id: customer.id,
+      name: customer.name,
+      address: customer.address
+    })
+    eventDispatcher.notify(customerAddressChangedEvent)
+
+    expect(eventHandlerSpy).toHaveBeenCalledWith(customerAddressChangedEvent)
+  })
+})
